5a922e60c8ca8759bc93e508d24c2ed7e14c82fa
sunil kumar sahoo <sunilsahoosks2002@gmail.com>
2024-10-13 20:03:52 +0530

    ..

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ const HeroSection = () => {
     return (
         <div className='text-center py-7 px-10 md:px-20  w-[100vw] gap-5 z-10'>
             <h1 className='text-[40px] md:text-6xl font-bold w-full md:w-[60%] md:mx-auto text-center '>Elevate Your Events at Liriq Venue</h1>
-            <p className='text-[16px] md:text-sm w-full md:w-[60%] mx-auto mt-4 text-justify md:text-pretty'>Discover the perfect setting for your next event at Liriq, where elegance meets functionality.
+            <p className='text-[13px] md:text-sm w-full md:w-[60%] font-normal mx-auto mt-4 text-center text-pretty'>Discover the perfect setting for your next event at Liriq, where elegance meets functionality.
                 Our premium space is designed to create unforgettable experiences for you and your guests.</p>
 
             {/* hero button section  */}
