98a90661b9ec65ff08381aade5b2a1009a6c313b
sunil kumar sahoo <sunilsahoosks2002@gmail.com>
2024-10-13 19:58:04 +0530

    ..

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -4,7 +4,7 @@ const HeroSection = () => {
     return (
         <div className='text-center py-7 px-10 md:px-20  w-[100vw] gap-5 z-10'>
             <h1 className='text-[40px] md:text-6xl font-bold w-full md:w-[60%] md:mx-auto text-center '>Elevate Your Events at Liriq Venue</h1>
-            <p className='text-[16px] md:text-sm w-full md:w-[60%] mx-auto mt-4 text-pretty'>Discover the perfect setting for your next event at Liriq, where elegance meets functionality.
+            <p className='text-[16px] md:text-sm w-full md:w-[60%] mx-auto mt-4 text-justify md:text-pretty'>Discover the perfect setting for your next event at Liriq, where elegance meets functionality.
                 Our premium space is designed to create unforgettable experiences for you and your guests.</p>
 
             {/* hero button section  */}
@@ -15,7 +15,7 @@ const HeroSection = () => {
 
             {/* //hero images section  */}
             <div className='w-full '>
-                <img className='h-[35rem] w-full mt-3 bg-cover bg-no-repeat bg-current'
+                <img className='h-[250px] rounded-md w-[410px] mx-auto md:h-[35rem] md:w-full mt-3 bg-cover bg-no-repeat bg-current'
                     src="/logo-svg.svg" alt='heroImage' />
 
             </div>
